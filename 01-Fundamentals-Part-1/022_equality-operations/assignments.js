d741715ba54e21f61007365900f9acfefa7b780e
Joh Leonhardt <johfleonhardt@gmail.com>
2020-11-02 21:20:26 +1100

    Equality Operators: == vs. ===

---
+++ 01-Fundamentals-Part-1/022_equality-operations/assignments.js
@@ -0,0 +1,69 @@
+//-----------022 Equality Operators: == vs. ===  ---------------------
+// LECTURE: Equality Operators: == vs. === 
+
+// 1.	Declare a variable 'numNeighbours' based on a prompt input like this: prompt('How many neighbour countries does your country have?'); 
+
+// 2.	If there is only 1 neighbour, log to the console 'Only 1 border!'(use loose equality == for now) 
+
+// 3.	Use an else-if block to log 'More than 1 border' in case 'numNeighbours' is greater than 1 
+
+// 4.	Use an else block to log 'No borders' (this block will be executed when 'numNeighbours' is 0 or any other value) 
+
+// 5.	Test the code with different values of'numNeighbours', including 1 and 0. 
+
+// 6.	Change == to ===, and test the code again, with the same values of 'numNeighbours'. Notice what happens when there is exactly 1 border! Why is this happening? 
+
+// 7.	Finally, convert 'numNeighbours' to a number, and watch what happens now when you input 1 
+
+// 8.	Reflect on why we should use the === operator and type conversion in this situation 
+
+//ANSWER: ---------------------------------------------------
+//1.)
+// const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+// console.log(numNeighbours)
+// //2.)
+// if (numNeighbours == 1) {
+//   console.log('Only 1 border!');
+//   //3.)
+// } else if (numNeighbours > 1) {
+//   console.log('More than 1 border.');
+//   //4.)
+// } else {
+//   console.log('No borders.');
+//}
+//5.) ---- see below:
+
+// 34
+// More than 1 border.
+
+// 1
+// Only 1 border!
+
+// 0ss
+// No borders.
+
+//6.)
+// const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+// console.log(numNeighbours)
+// if (numNeighbours === 1) {
+//   console.log('Only 1 border!');
+// } else if (numNeighbours > 1) {
+//   console.log('More than 1 border.');
+// } else {
+//   console.log('No borders.');
+// }
+
+//7.) OK I DON"T KNOW HOW TO DO THIS !!!!!!!!!
+const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+console.log(numNeighbours)
+if (numNeighbours === 1) {
+  console.log('Only 1 border!');
+} else if (numNeighbours > 1) {
+  console.log('More than 1 border.');
+} else {
+  console.log('No borders.');
+};
+numNeighbours = 5;
+
+// 8.	
+// Answer: Because === has less bug's then ==
---
+++ 01-Fundamentals-Part-1/022_equality-operations/equality-operations.js
@@ -0,0 +1,103 @@
+// const age = 18;
+// if (age === 18) console.log('You just become an adult');
+// // result = You just become an adult
+
+//--------------------------------------------------------------
+
+// const age = 19;
+// if (age === 18) console.log('You just become an adult');
+// // NO result =
+
+//--------------------------------------------------------------
+
+// const age = 18;
+// if (age === 18) console.log('You just become an adult');
+// // result = You just become an adult
+
+//--------------------------------------------------------------
+
+// // 18 === 18 // true as (number) = (number) = true
+// // 18 === 19 // false as (numbers are different) = so false
+// // '18' === 19 // false as (string) = (number) = flase 
+// // '18' == 19 // false as (string) = (number) = false
+// '18' == 19   // false
+// '18' === 19  // false
+// 18 == 18     // true
+// 19 == '19'   // true
+// '1' == '1'   // true
+// '1' == 1     // true
+
+//--------------------------------------------------------------
+
+// const age = 18;
+// if (age === 18) console.log('You just become an adult! (strict)'); // will run
+// if (age == 18) console.log('You just become an adult! (loose)'); // will run
+//--------------------------------------------------------------
+// const age = '18';
+// if (age === 18) console.log('You just become an adult! (strict)'); // will NOT run
+// if (age == 18) console.log('You just become an adult! (loose)'); // will run
+
+//--------------------------------------------------------------
+// ===== ===== ==== ==== ==== ==== ===== ===== ===== ====== ===== ===
+// =====  RULE:  AVOID USING THE == OPERATOR (2x =) RULE: ===========
+//  OPERATOR Double == will do type conversion
+//  OPRATOR Tripple === won't do type conversion
+
+//--------------------------------------------------------------
+
+// prompt("What's your favourite number?"); // will bring up promt with this question
+
+//--------------------------------------------------------------
+
+// const favourite = prompt("What's your favourite number?");
+// console.log(favourite); // this will print what ever number you input now
+// console.log(typeof favourite); // will print the word: string
+// if (favourite == 23) {
+//   console.log('Cool 23 is a amazing number!') // will print: Cool 23 is a amazing number!
+
+//--------------------------------------------------------------
+
+// const favourite = prompt("What's your favourite number?");
+// if (favourite == 22) {    // '22' == 22 --> TRUE
+//   console.log('Cool 23 is a amazing number!')
+
+//   //--------------------------------------------------------------
+
+//   const favourite = Number(prompt("What's your favourite number?"));
+//   if (favourite == 23) {    // '23' == 23 --> TRUE
+//     console.log('Cool 23 is a amazing number!')
+
+//--------------------------------------------------------------
+
+//     const favourite = Number(prompt("What's your favourite number?"));
+//     if (favourite === 23) {    // 23 === 23 --> TRUE // 23 is also a (number here)
+//       console.log('Cool 23 is a amazing number!')
+
+//--------------------------------------------------------------
+
+//       const favourite = Number(prompt("What's your favourite number?"));
+//       if (favourite === 23) {    // '23' === 23 --> FALSE
+//         console.log('Cool 23 is a amazing number!')
+
+//--------------------------------------------------------------
+
+//         const favourite = Number(prompt("What's your favourite number?"));
+//         if (favourite === 23) {    // 22 === 23 --> FALSE
+//           console.log('Cool 23 is a amazing number!')
+
+//--------------------------------------------------------------
+
+const favourite = Number(prompt("What's your favourite number?"));
+if (favourite === 23) {    // 22 === 23 --> FALSE
+  console.log('Cool 23 is a amazing number!')
+} else if (favourite === 7) {
+  console.log('7 is also a cool number.')
+} else if (favourite === 9) {
+  console.log('9 is also a cool number.')
+} else {
+  console.log('This number is not 23 or 7 or 9.')
+}
+
+if (favourite !== 23) console.log('Why not 23?'); // !== is the lose version
+
+// != is the strict version
