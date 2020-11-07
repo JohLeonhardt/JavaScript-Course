74416e878b8ea61a48c1abad9d583d99abff6b76
Joh Leonhardt <johfleonhardt@gmail.com>
2020-11-07 11:53:38 +1100

    statements-and-expressions

---
+++ 01-Fundamentals-Part-1/027_statements-and-expressions/statements-and-expressions.js
@@ -0,0 +1,14 @@
+// These are all EXPRESSIONS
+3 + 4
+1991
+true && false && !false
+
+// This is a STATMENT
+if (23 > 10) {
+  const str = '23 is bigger'; // this string here => '23 is bigger' is a EXPRESSION
+}
+
+// RULE: When something ends in a => ; then it is a STATMENT
+
+const me = 'Jonas';
+console.log(`I'm ${2037 - 1991} years old. ${me}`); // so => ${2037 - 1991} OR ${me} are both EXPRESSIONS while this => `I'm         years old.`is a STATMENT
