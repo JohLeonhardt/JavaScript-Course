ea1968a0b9c45d4906b4bc1367fa8e103528383b
Joh Leonhardt <johfleonhardt@gmail.com>
2020-11-09 21:37:49 +1100

    0JavaScript Fundamentals – Part 2 - Activating Strict Mode

---
+++ 02-Fundamentals-Part-2/031_activating-strict-mode/activating-strict-mode.js
@@ -0,0 +1,11 @@
+'use strict'; // THIS IS HOW YOU ACTIVATE STRICT MODE
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log('I can drive.');
+
+// const interface = 'Audio';
+// const pritvate = 543;
+const if = 23;
