0a87169baeec82294879c01d584ef376161ea052
Joh Leonhardt <johfleonhardt@gmail.com>
2020-11-13 15:39:26 +1100

    Introduction to Arrays

--- 02-Fundamentals-Part-2/039_introduction-to-arrays/assignments.js
+++ 02-Fundamentals-Part-2/039_introduction-to-arrays/assignments.js
@@ -1,23 +1,25 @@
 'use strict';
-//-----------036 Functions Calling Other Functions  ---------------------
-// LECTURE: Functions Calling Other Functions 
+//-----------039 Introduction to Arrays   ---------------------
 
-// 1.	Create a function called 'describePopulation'. Use the function type you like the most. This function takes in two arguments: 'country' and 'population', and returns a string like this: 'China has 1441 million people, which is about 18.2% of the world.' 
+// LECTURE: Introduction to Arrays 
 
-// 2.	To calculate the percentage, 'describePopulation' call the 'percentageOfWorld1' you created earlier 
+// 1.	Create an array containing 4 population values of 4 countries of your choice. You may use the values you have been using previously. Store this array into a variable called 'populations' 
 
-// 3.	Call 'describePopulation' with data for 3 countries of your choice. 
+// 2.	Log to the console whether the array has 4 elements or not (true or false) 
 
-//------ ANSWEAR -----------------
+// 3.	Create an array called 'percentages' containing the percentages of the world population for these 4 population values. Use the function 'percentageOfWorld1' that you created earlier to compute the 4 percentage values 
 
-function percentageOfWorld1(population) {
-  return (population / 7900) * 100;
+//------ ANSWEAR --------
+//1
+const populations = [1141, 25, 83, 5];
+//2
+console.log(populations < 4);
+//3
+const percentages = function (percentageOfWorld1) {
+  return (percentageOfWorld1 / 7900) * 100;
 }
-const describePopulation = function (country, population) {
-  const percentage = percentageOfWorld1(population);
-  const description = `${country} has ${population} millions people, which is about ${percentage}% of the world.`;
-  console.log(description);
-}
-describePopulation('Australia', 25);
-describePopulation('China', 1441);
-describePopulation('Germany', 83);
+const china = percentages(populations[0]);
+const australia = percentages(populations[1]);
+const germany = percentages(populations[2]);
+const newZealand = percentages(populations[3]);
+console.log(china, australia, germany, newZealand);
