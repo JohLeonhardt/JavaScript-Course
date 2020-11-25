db1c32f82a316d3c974743eb8e700d70cd0ae051
Joh Leonhardt <johfleonhardt@gmail.com>
2020-11-25 20:52:43 +1100

    Introduction to Objects

---
+++ 02-Fundamentals-Part-2/042_introduction-to-objects/assignments.js
@@ -0,0 +1,15 @@
+'use strict';
+//-----------041 LECTURE: Introduction to Objects  ---------------------
+
+// LECTURE: Introduction to Objects 
+
+// 1. Create an object called 'myCountry' for a country of your choice, containing properties 'country', 'capital', 'language', 'population' and 'neighbours' (an array like we used in previous assignments) 
+
+
+const myCountry = {
+  country: 'Australia',
+  capital: 'Cambera',
+  language: 'English',
+  population: 25,
+  neighbours: []
+}
