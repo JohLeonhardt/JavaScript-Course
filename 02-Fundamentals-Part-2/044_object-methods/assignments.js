cb341c529c48b57cf07c11082ad391422e9fd70a
Joh Leonhardt <johfleonhardt@gmail.com>
2020-11-28 12:33:17 +1100

    Object Methods

--- 02-Fundamentals-Part-2/044_object-methods/assignments.js
+++ 02-Fundamentals-Part-2/044_object-methods/assignments.js
@@ -1,26 +1,32 @@
 'use strict';
-//-----------043 LECTURE: Dot vs. Bracket Notation   ---------------------
+//-----------044 LECTURE: Object Methods    ---------------------
 
-// LECTURE: Dot vs. Bracket Notation 
+// LECTURE: Object Methods 
+// 1.	Add a method called 'describe' to the 'myCountry' object. This method will log a string to the console, similar to the string logged in the previous assignment, but this time using the 'this' keyword. 
 
-// 1.	Using the object from the previous assignment, log a string like this to the console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries and a capital called Helsinki.' 
+// 2.	Call the 'describe' method 
 
-// 2.	Increase the country's population by two million using dot notation, and then decrease it by two million using brackets notation. 
+// 3.	Add a method called 'checkIsland' to the 'myCountry' object. This method will set a new property on the object, called 'isIsland'. 'isIsland' will be true if there are no neighbouring countries, and false if there are. Use the ternary operator to set the property. 
+
 
 const myCountry = {
   country: 'Australia',
-  population: 25,
-  language: 'English',
-  neighbours: ['New Zealand', 'Indonesia', 'More Islands'],
   capital: 'Cambera',
-}
-
-console.log(
-  `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`
-);
-myCountry.population += 2;
-console.log(myCountry.population);
-
-myCountry['population'] -= 2;
-console.log(myCountry.population);
-
+  language: 'english',
+  population: 25,
+  neighbours: ['New Zealand', 'Papua New Guinea', 'Indonesia', 'East Timor', 'The Solomon Islands', 'Vanuatu'],
+  //1 & 2
+  describe: function () {
+    console.log(`${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`
+    );
+  },
+  //3
+  checkIsland: function () {
+    this.isIsland = this.neighbours.length === 0 ? 'true' : 'false';
+    // Even simpler version (see why this works...)
+    // this.isIsland = !Boolean(this.neighbours.length);
+  }
+};
+myCountry.describe();
+// myCountry.checkIsland();
+// console.log(myCountry);
--- 02-Fundamentals-Part-2/044_object-methods/object-methods.js
+++ 02-Fundamentals-Part-2/044_object-methods/object-methods.js
@@ -1,7 +1,45 @@
 'use strict';
 
 const jonas = {
-  firstName: 'Jonas'
-}
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  birthYear: 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven'],
+  hasDriversLicense: true,
 
+  //OBJECT METHOD VERSION 1
+  // calcAge: function (birthYear) {
+  //   return 2037 - birthYear;
+  // }
 
+  //OBJECT METHOD VERSION 2
+  // calcAge: function () {
+  //   // console.log(this);
+  //   return 2037 - this.birthYear;
+  // }
+
+  //OBJECT METHOD VERSION 3 - storing a new property here "age" (creating a new property (age) on the current object (the object is calcAge here))
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+  //NEW METHOD FROM CHALLENGE below
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} diver's license.`
+  }
+};
+
+console.log(jonas.calcAge());
+
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+// CHALLENGE
+// "Jonas is a 46-year old teacher, and he has a driver's license."
+
+//My try outcome: Jonas is a 46-year old teacher, and he has true driver's license.
+// console.log(`${jonas.firstName} is a ${jonas.age}-year old ${jonas.job}, and he has ${jonas.hasDriversLicense} driver's license.`);
+
+console.log(jonas.getSummary());
